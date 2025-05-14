fd13e8d3246d31a67f61f36cd4bf4236becd0da1
Marc Prange <mp1506@stellar-amd.princeton.edu>
2025-05-14 09:55:47 -0400

    Make global map of high-CAPE occurrences in 3 regions.

--- hk25-Land/cape_maps.py
+++ hk25-Land/cape_maps.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import dask.array as da
 import numcodecs
 import healpix as hp
+import healpy as hpy
+from healpy.visufunc import projscatter
 import easygems.healpix as egh
 import easygems.remap as egr
 import intake     # For catalogs
@@ -16,8 +18,9 @@ def worldmap(var, **kwargs):
     #projection = ccrs.Robinson(central_longitude=-135.5808361)
     projection = ccrs.Robinson(central_longitude=0)
     fig, ax = plt.subplots(
-        figsize=(8, 4), subplot_kw={"projection": projection}, 
-        constrained_layout=True
+        figsize=(8, 4), 
+        constrained_layout=True,
+        subplot_kw={"projection": projection}
     )
     ax.set_global()
 
@@ -107,18 +110,55 @@ def find_regional_cape_maxima(regional_cape_da, n_cases=10):
 #%%
 if __name__ == "__main__":
     zoom = 5
-    ds = load_native_xsh24_data(zoom=zoom).sel(time='2020')
-    daily_max_cape = ds.CAPE_max.groupby('time.dayofyear').max()
+    ds = load_native_xsh24_data(zoom=zoom).sel(time=slice('2020-01-01', '2020-12-31'))
+    daily_max_cape = ds.CAPE_max.groupby('time.dayofyear').max('time')
     daily_max_cape_97 = get_quantile(daily_max_cape.load(), 0.97)
     #%%
     # Select region
-    extent = [-20, 40, 30, 60]  # Europe
-    icell, region = get_healpix_region(extent, zoom)
-    ds_region = ds.isel(cell=icell)
-    regional_cape_maxima = find_regional_cape_maxima(
-        ds_region.CAPE_max.stack(case=['cell', 'time']))
+    extent_sa = [35, 60, 10, 35]  # Saudi Arabia
+    extent_na = [-110, -70, 30, 55]
+    extent_au = [140, 170, -50, -25]
+    icell, region = get_healpix_region(extent_sa, zoom)
+    ds_sa = ds.isel(cell=icell)
+    icell, region = get_healpix_region(extent_na, zoom)
+    ds_na = ds.isel(cell=icell)
+    icell, region = get_healpix_region(extent_au, zoom)
+    ds_au = ds.isel(cell=icell)
+    sa_cape_maxima = find_regional_cape_maxima(
+        ds_sa.CAPE_max.stack(case=['cell', 'time']),
+        n_cases=50)
+    na_cape_maxima = find_regional_cape_maxima(
+        ds_na.CAPE_max.stack(case=['cell', 'time']),
+        n_cases=50)
+    au_cape_maxima = find_regional_cape_maxima(
+        ds_au.CAPE_max.stack(case=['cell', 'time']),
+        n_cases=50)
     #%%
-    fig, ax = worldmap(daily_max_cape_97)
-    ax.scatter(regional_cape_maxima.lon.values, regional_cape_maxima.lat.values, 
-               c=regional_cape_maxima.values, cmap='RdYlBu_r')
+    fig, ax = plt.subplots(
+        subplot_kw={"projection": ccrs.PlateCarree()},
+        figsize=(10, 10))
+    ax.set_global()
+    s = egh.healpix_show(daily_max_cape_97, ax=ax)
+    plt.colorbar(s, ax=ax, orientation='vertical', 
+                 pad=0.05, shrink=0.3, label='P97 CAPE [J/kg]')
+    plot_extent_box(ax, extent_sa, edgecolor="y", linewidth=2)
+    ax.scatter(
+        sa_cape_maxima.lon, sa_cape_maxima.lat, 
+        c=sa_cape_maxima, cmap='Reds', s=5, 
+        vmin=0, vmax=7000, alpha=0.5)
+    plot_extent_box(ax, extent_na, edgecolor="c", linewidth=2)
+    ax.scatter(
+        na_cape_maxima.lon, na_cape_maxima.lat, 
+        c=na_cape_maxima, cmap='Reds', s=5, 
+        vmin=0, vmax=7000, alpha=0.5)
+    plot_extent_box(ax, extent_au, edgecolor="m", linewidth=2)
+    s = ax.scatter(
+        au_cape_maxima.lon, au_cape_maxima.lat, 
+        c=au_cape_maxima, cmap='Reds', s=5, 
+        vmin=0, vmax=7000, alpha=0.5)
+    ax.coastlines()
+    plt.colorbar(s, ax=ax, orientation='vertical', 
+                 pad=0.01, shrink=0.3, 
+                 label='max. daily CAPE [J/kg]')
+
     # %%
