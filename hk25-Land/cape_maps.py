340852d0d1211dee663e0963db0dd9251ad5640c
Marc Prange <mp1506@stellar-amd.princeton.edu>
2025-05-14 14:56:24 -0400

    Make event selection work better for high-res data.

--- hk25-Land/cape_maps.py
+++ hk25-Land/cape_maps.py
@@ -105,11 +105,34 @@ def get_quantile(da, q):
 
 def find_regional_cape_maxima(regional_cape_da, n_cases=10):
     """Find the n_cases with the highest cape in the regional_cape_da."""
-    return regional_cape_da.sortby(regional_cape_da, ascending=False).isel(case=slice(0, n_cases))
+    # Group by time and get max cape for each day
+    daily_max = regional_cape_da.resample(time='1D').max().max('cell')
+    sorted_idx = np.argsort(daily_max.values)[-n_cases:][::-1]
+    top_times = daily_max['time'].values[sorted_idx]
+    top_events = []
+    for day in top_times:
+        day_data = regional_cape_da.sel(time=day)
+        idx = day_data.argmax('cell').compute().item()
+        top_events.append({
+            'lon': day_data.lon.isel(cell=idx).compute().item(),
+            'lat': day_data.lat.isel(cell=idx).compute().item(),
+            'cape': day_data.isel(cell=idx).compute().item(),
+            'date': str(day)[:10]
+        })
+    events_da = xr.DataArray(
+        data=np.array([e['cape'] for e in top_events]),
+        coords={
+            'lon': ('event', [e['lon'] for e in top_events]),
+            'lat': ('event', [e['lat'] for e in top_events]),
+            'date': ('event', [e['date'] for e in top_events])
+        },
+        dims=['event']
+    )
+    return events_da
 
 #%%
 if __name__ == "__main__":
-    zoom = 5
+    zoom = 8
     ds = load_native_xsh24_data(zoom=zoom).sel(time=slice('2020-01-01', '2020-12-31'))
     daily_max_cape = ds.CAPE_max.groupby('time.dayofyear').max('time')
     daily_max_cape_97 = get_quantile(daily_max_cape.load(), 0.97)
@@ -125,40 +148,77 @@ if __name__ == "__main__":
     icell, region = get_healpix_region(extent_au, zoom)
     ds_au = ds.isel(cell=icell)
     sa_cape_maxima = find_regional_cape_maxima(
-        ds_sa.CAPE_max.stack(case=['cell', 'time']),
-        n_cases=50)
+        ds_sa.CAPE_max,
+        n_cases=20)
     na_cape_maxima = find_regional_cape_maxima(
-        ds_na.CAPE_max.stack(case=['cell', 'time']),
-        n_cases=50)
+        ds_na.CAPE_max,
+        n_cases=20)
     au_cape_maxima = find_regional_cape_maxima(
-        ds_au.CAPE_max.stack(case=['cell', 'time']),
-        n_cases=50)
+        ds_au.CAPE_max,
+        n_cases=20)
     #%%
-    fig, ax = plt.subplots(
+    fig, axs = plt.subplots(nrows=2, ncols=2,
         subplot_kw={"projection": ccrs.PlateCarree()},
-        figsize=(10, 10))
-    ax.set_global()
-    s = egh.healpix_show(daily_max_cape_97, ax=ax)
-    plt.colorbar(s, ax=ax, orientation='vertical', 
-                 pad=0.05, shrink=0.3, label='P97 CAPE [J/kg]')
-    plot_extent_box(ax, extent_sa, edgecolor="y", linewidth=2)
-    ax.scatter(
+        figsize=(20, 15))
+    scatter_cmap = 'YlGnBu'
+    axs[0, 0].set_global()
+    c = egh.healpix_show(daily_max_cape_97, ax=axs[0, 0], cmap='Reds')
+    plot_extent_box(axs[0, 0], extent_sa, edgecolor="g", linewidth=2)
+    axs[0, 0].scatter(
         sa_cape_maxima.lon, sa_cape_maxima.lat, 
-        c=sa_cape_maxima, cmap='Reds', s=5, 
+        c=sa_cape_maxima, cmap=scatter_cmap, s=10, 
         vmin=0, vmax=7000, alpha=0.5)
-    plot_extent_box(ax, extent_na, edgecolor="c", linewidth=2)
-    ax.scatter(
+    plot_extent_box(axs[0, 0], extent_na, edgecolor="c", linewidth=2)
+    axs[0, 0].scatter(
         na_cape_maxima.lon, na_cape_maxima.lat, 
-        c=na_cape_maxima, cmap='Reds', s=5, 
+        c=na_cape_maxima, cmap=scatter_cmap, s=10, 
         vmin=0, vmax=7000, alpha=0.5)
-    plot_extent_box(ax, extent_au, edgecolor="m", linewidth=2)
-    s = ax.scatter(
+    plot_extent_box(axs[0, 0], extent_au, edgecolor="m", linewidth=2)
+    s = axs[0, 0].scatter(
         au_cape_maxima.lon, au_cape_maxima.lat, 
-        c=au_cape_maxima, cmap='Reds', s=5, 
+        c=au_cape_maxima, cmap=scatter_cmap, s=10, 
         vmin=0, vmax=7000, alpha=0.5)
-    ax.coastlines()
-    plt.colorbar(s, ax=ax, orientation='vertical', 
-                 pad=0.01, shrink=0.3, 
-                 label='max. daily CAPE [J/kg]')
+    
+    axs[0, 1].set_extent(extent_na)
+    egh.healpix_show(daily_max_cape_97, ax=axs[0, 1], cmap='Reds')
+    axs[0, 1].scatter(
+        na_cape_maxima.lon, na_cape_maxima.lat, 
+        c=na_cape_maxima, cmap=scatter_cmap, s=100, 
+        vmin=0, vmax=7000, alpha=0.5)
+    
+    axs[1, 0].set_extent(extent_sa)
+    egh.healpix_show(daily_max_cape_97, ax=axs[1, 0], cmap='Reds')
+    axs[1, 0].scatter(
+        sa_cape_maxima.lon, sa_cape_maxima.lat, 
+        c=sa_cape_maxima, cmap=scatter_cmap, s=100, 
+        vmin=0, vmax=7000, alpha=0.5)
+
+    axs[1, 1].set_extent(extent_au)
+    egh.healpix_show(daily_max_cape_97, ax=axs[1, 1], cmap='Reds')
+    axs[1, 1].scatter(
+        au_cape_maxima.lon, au_cape_maxima.lat, 
+        c=au_cape_maxima, cmap=scatter_cmap, s=100, 
+        vmin=0, vmax=7000, alpha=0.5)
+    
+    # Create two axes for colorbars on the right side
+    cbar_ax1 = fig.add_axes([0.92, 0.55, 0.02, 0.3])  # For first colorbar
+    cbar_ax2 = fig.add_axes([0.92, 0.15, 0.02, 0.3])  # For second colorbar
+    
+    # Add colorbars to the new axes
+    plt.colorbar(c, cax=cbar_ax1, orientation='vertical')
+    plt.colorbar(s, cax=cbar_ax2, orientation='vertical')
+    cbar_ax1.tick_params(labelsize=12)
+    cbar_ax2.tick_params(labelsize=12)
+    cbar_ax1.set_ylabel('P97 of daily max. CAPE [J/kg]', fontsize=12)
+    cbar_ax2.set_ylabel('peak CAPE [J/kg]', fontsize=12)
+    for ax, color in zip(axs.flatten(), ['k', 'c', 'g', 'm']):
+        ax.coastlines()
+        for spine in ax.spines.values():
+            spine.set_color(color)
+            spine.set_linewidth(6)
+    # plt.tight_layout()
+    # plt.colorbar(s, ax=ax, orientation='vertical', 
+    #              pad=0.01, shrink=0.3, 
+    #              label='max. daily CAPE [J/kg]')
 
     # %%
